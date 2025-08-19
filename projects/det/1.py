e15c3d5df401b9b7f84a4b5d758dd57a5fcd41e8
zycskylove520 <867245713@qq.com>
2025-08-19 13:36:10 +0800

    update yolov8 classify model

--- projects/det/1.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from tinytrain.modules import ResNetLayer
-
-if __name__ == '__main__':
-    x = torch.randn(1, 64, 640, 640)
-    layer = ResNetLayer(64, 256, n=2)
-    y = layer(x)
-    print(y.shape)
