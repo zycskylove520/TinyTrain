1cc727d9003cd9fd296ffbc496c16194495d17b8
zycskylove520 <867245713@qq.com>
2025-11-24 11:26:02 +0800

    移除测试文件

--- tinytrain/models/clip/__init__.py
+++ tinytrain/models/clip/__init__.py
@@ -19,21 +19,3 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
-name = "YOLOv11-cls"
-scale = "n"
-
-scales = {
-    "n": {"depth": 0.25, "summary": "151 layers, 1543914 parameters, 1543914 gradients, 0.4 GFLOPs, for a 224×224 input."},
-    "s": {"depth": 0.50, "summary": "xxxx"}
-}
-
-network = {
-    0: {"type": "entry",
-        "module": "CBA",
-        "repeat": 1,
-        "from": -1,
-        "args": {"in_channels": 3, "out_channels": 64, "kernel_size": 3, "stride": 2}
-        },
-    1: {"type": "entry",}
-}
